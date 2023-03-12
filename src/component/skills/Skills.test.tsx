7046be40027bb49cff34897e7e71fdddad3ac6ce
Mr.Yogesh <yogeshvanzara98@gmail.com>
2023-03-12 19:19:52 +0530

    getby/queryby/findby

--- src/component/skills/Skills.test.tsx
+++ src/component/skills/Skills.test.tsx
@@ -14,4 +14,33 @@ describe("Skills", () => {
     const listItemElements = screen.getAllByRole("listitem");
     expect(listItemElements).toHaveLength(skills.length);
   });
+  test("renders login button", () => {
+    render(<Skills skills={skills} />);
+    const loginButton = screen.getByRole("button", {
+      name: "Login",
+    });
+    expect(loginButton).toBeInTheDocument();
+  });
+  //*for not in dom we do queryByRole
+  test("Start Learning button is not rendered", () => {
+    render(<Skills skills={skills} />);
+    const startLearningButton = screen.queryByRole("button", {
+      name: "Start learning",
+    });
+    expect(startLearningButton).not.toBeInTheDocument();
+  });
+  //*after some time display not displayed:findBy & findAllBy:default time 1000ms
+  test("Start Learning button is eventually displayed", async () => {
+    render(<Skills skills={skills} />);
+    const startLearningButton = await screen.findByRole(
+      "button",
+      {
+        name: "Start learning",
+      },
+      {
+        timeout: 2000,
+      }
+    );
+    expect(startLearningButton).toBeInTheDocument();
+  });
 });
