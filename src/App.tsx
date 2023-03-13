0c72be92373de2d20bc459e9e0c1cd0c571aa867
Mr.Yogesh <yogeshvanzara98@gmail.com>
2023-03-13 16:27:03 +0530

    mock function

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import { Application } from "./component/application/Application";
+import { CounterTwo } from "./component/counter-two/CounterTwo";
 import { Counter } from "./component/counter/Counter";
 import { MuiMode } from "./component/mui/MuiMode";
 import { Skills } from "./component/skills/Skills";
@@ -13,7 +14,8 @@ function App() {
         {/* <Application /> */}
         {/* <Skills skills={["HTML", "CSS", "JavaScript"]} /> */}
         {/* <Counter /> */}
-        <MuiMode />
+        {/* <MuiMode /> */}
+        <CounterTwo count={1} />
       </div>
     </AppProviders>
   );
